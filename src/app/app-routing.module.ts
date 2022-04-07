a46ef8d373e6749b87ef5581772defe4cf238422
wisdomstar94 <wisdomstar94@gmail.com>
2022-04-07 22:53:56 +0900

    1) / 으로 접근시 /test 로 리다이렉트 되게 변경 2) swiper 에서 autoplay 테스트

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'test',
+    pathMatch: 'prefix',
     loadChildren: () => import('./pages/test/test.module').then(m => m.TestModule)
-  }
+  },
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: '/test',
+  },
+
 ];
 
 @NgModule({
--- src/app/components/boxes/swiper-custom/swiper-custom.component.ts
+++ src/app/components/boxes/swiper-custom/swiper-custom.component.ts
@@ -1,17 +1,19 @@
-import { AfterContentChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, OnInit, QueryList, TemplateRef } from '@angular/core';
-import SwiperCore, { Swiper, SwiperOptions, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, OnInit, QueryList, TemplateRef, ViewChild } from '@angular/core';
+import SwiperCore, { Swiper, SwiperOptions, Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 import { SwiperSlideCustomDirective } from './directives/swiper-slide-custom.directive';
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 @Component({
   selector: 'app-swiper-custom',
   templateUrl: './swiper-custom.component.html',
   styleUrls: ['./swiper-custom.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SwiperCustomComponent implements OnInit, AfterContentChecked {
+export class SwiperCustomComponent implements OnInit {
   // style.scss 에 반드시 @import 'swiper/scss'; 을 넣어주세요!!
   @ContentChildren(SwiperSlideCustomDirective, { read: TemplateRef }) templates!: QueryList<TemplateRef<any>>;
+  @ViewChild('swiper') swiper!: SwiperComponent;
   config: SwiperOptions;
 
   constructor(
@@ -20,7 +22,7 @@ export class SwiperCustomComponent implements OnInit, AfterContentChecked {
     this.config = {
       direction: 'horizontal',
       spaceBetween: 0,
-      navigation: false,
+      navigation: true,
       pagination: {
         clickable: true,
       },
@@ -28,12 +30,11 @@ export class SwiperCustomComponent implements OnInit, AfterContentChecked {
         draggable: true,
       },
       loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false, // 쓸어 넘기거나 버튼 클릭 시 자동 슬라이드 정지.
+      },
     };
-  }
-
-  ngAfterContentChecked(): void {
-    console.log('SwiperCustomComponent.templates', this.templates);
-    this.changeDetectorRef.detectChanges();
   }
 
   ngOnInit(): void {
@@ -54,6 +55,8 @@ export class SwiperCustomComponent implements OnInit, AfterContentChecked {
   }
 
   onSlideChange([swiper]: Swiper[]): void {
-    console.log('onSlideChange.swiper', swiper);
+    // console.log('onSlideChange.swiper', swiper);
+    const realIndex = swiper.realIndex;
+    console.log('onSlideChange.realIndex', realIndex);
   }
 }
